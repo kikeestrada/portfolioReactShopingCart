559ecb601d7a7233309fbd2ae3836de2be1d749f
Enrique Jose Estrada Nunez <kikeestrada@gmail.com>
2019-11-05 22:17:18 -0600

    Updates

--- precache-manifest.50c04b538d78f8e6c058c92f2ddd94f7.js
+++ precache-manifest.50c04b538d78f8e6c058c92f2ddd94f7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "45cff311ce4753cfea8bc82f71d562f7",
+    "revision": "d115ddc81d40f8472dc02e7d9110415c",
     "url": "/4geektest/index.html"
   },
   {
-    "revision": "e0020612b70509e38ec6",
+    "revision": "c93a25dd4e85b93ea36a",
     "url": "/4geektest/static/css/main.2482b1e9.chunk.css"
   },
   {
-    "revision": "4070c2aecd086adbec6d",
-    "url": "/4geektest/static/js/2.35da1efe.chunk.js"
+    "revision": "3a4cc250715e250d2b04",
+    "url": "/4geektest/static/js/2.cc0e0575.chunk.js"
   },
   {
-    "revision": "e0020612b70509e38ec6",
-    "url": "/4geektest/static/js/main.7199b49b.chunk.js"
+    "revision": "c93a25dd4e85b93ea36a",
+    "url": "/4geektest/static/js/main.b0211fac.chunk.js"
   },
   {
     "revision": "4c952051ad39f13f1baa",
