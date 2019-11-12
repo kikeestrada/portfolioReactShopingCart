ec07cf450df78a166a94e7a9e98fbd6dfa9665d8
Enrique Jose Estrada Nunez <kikeestrada@gmail.com>
2019-11-12 14:11:51 -0600

    ajustes con el contador de compras

--- src/components/atoms/CartCounter.jsx
+++ src/components/atoms/CartCounter.jsx
@@ -3,10 +3,10 @@ import { connect } from "react-redux"
 
 
 const CartCounter = ({cartLegth}) => (
-    <span className="s-card s-cross-center s-main-end">
+    <li className="s-card">
         <i className="fas fa-cart-arrow-down"></i>
         <span className="s-card__number"> {`${cartLegth}`} </span>
-    </span>
+    </li>
 )
 
 const mapStateToProps = state => (
--- src/components/organisms/MainMenu.jsx
+++ src/components/organisms/MainMenu.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {createRef} from 'react'
 import CartCounter from '../atoms/CartCounter'
 
+const menu = createRef()
+const toggleMenu = ()=> menu.current.classList.toggle('show');
 
 const MainMenu = () => (
     <header className="main-header">
@@ -8,8 +10,18 @@ const MainMenu = () => (
             <div className="s-cols-4 lg-cols-1 s-cross-center">
                     <img className="main-logo" src="img/logo.png" alt="algo" />
             </div>
-            <CartCounter/>
+            <div className="s-grid-1 lg-cols-3 s-cross-center s-main-end header-links">
+                <nav className="main-menu" ref={menu}>
+                    <ul>
+                        <CartCounter/>
+                    </ul>
+                </nav>
+                <div className="main-menu-toggle to-l" id="main-menu-toggle"
+                     onClick={() => toggleMenu()}
+                >
 
+                </div>
+            </div>
         </div>
     </header>
 );
